cf6c44008f6a2cafaf67729fa21edccce441adf0
JaeHwan ma <kohkss@naver.com>
2020-02-28 11:23:16 +0900

    임시 비밀번호 발송

--- src/main/java/icia/project/gabom/SendUserPasswordController.java
+++ src/main/java/icia/project/gabom/SendUserPasswordController.java
@@ -2,23 +2,27 @@ package icia.project.gabom;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import icia.project.gabom.dto.Member;
+import icia.project.gabom.service.SendUserPassword;
 
 @Controller
 public class SendUserPasswordController {
 
+	@Autowired
+	SendUserPassword sendUserPassword;
+	
 	
 	@PostMapping(value = "/senduserpassword")
 	public String sendUserPassword(Member member,RedirectAttributes rttr){
-		//Map<String, String> sucessMessage=sendUserId.sendUserId(member.getMember_email());
-		//rttr.addFlashAttribute("message", sucessMessage);
-		//return "redirect:/login";
 		System.out.println("패스워드 아이디"+member.getMember_id());
 		System.out.println("패스워드 이메일"+member.getMember_email());
-		return null;
+		Map<String, String> sucessMessage=sendUserPassword.sendUserpassword(member);
+		rttr.addFlashAttribute("message", sucessMessage);
+		return "redirect:/login";
 	}
 }
