bf83cb1d03c6ed64677e877f6fb447c50962b7bf
Kim-seongjun <01090524238@naver.com>
2020-03-03 18:50:08 +0900

    20.03.03 승인/거절 완

--- src/main/java/icia/project/gabom/service/AdminjudgeManagement.java
+++ src/main/java/icia/project/gabom/service/AdminjudgeManagement.java
@@ -17,48 +17,73 @@ public class AdminjudgeManagement {
 	@Autowired
 	private IAdminDao aDao;
 	private ModelAndView mav;
-	
+
+	// adminmenu.jsp 접속시 house,food 출력
 	public ModelAndView housejudgelist() {
 		mav = new ModelAndView();
-		List<Adminhouse> hlist = aDao.getHouseList(); //house 등록타입0번인 리스트 담아오기
-		System.out.println("List="+hlist);
-		System.out.println("List="+hlist.get(0));
-		System.out.println("hlist="+hlist.size());
-		
-		List<Adminfood> flist = aDao.getFoodList(); //food 등록타입 0번인 리스트 담아오기
-		System.out.println("flist="+flist);
-		
-		
+
+		List<Adminhouse> hlist = aDao.getHouseList(); // house 등록타입0번인 리스트 담아오기
+		List<Adminfood> flist = aDao.getFoodList(); // food 등록타입 0번인 리스트 담아오기
+
+		System.out.println("List=" + hlist);
+		System.out.println("flist=" + flist);
+
 		String hson = new Gson().toJson(hlist);
-		
 		String fson = new Gson().toJson(flist);
-		
-		
+
 		mav.addObject("housejudge", hson);
 		mav.addObject("foodjudge", fson);
-		System.out.println("hson="+hson);
-		System.out.println("fson="+fson);
-		//mav.addObject("gg", food);
-		
+		System.out.println("hson=" + hson);
+		System.out.println("fson=" + fson);
+
 		mav.setViewName("adminmenu");
 		return mav;
-	} //housejudgelist End
+	} // housejudgelist End
 
-	//미사용
-//	public ModelAndView detailjudge() {
-//		mav = new ModelAndView();
-//		//이프 타입.이퀄"house","food"
-//		List<String> jlist = aDao.getjudgedetail();
-//		return null;
-//	}
-	//미사용
-//	public ModelAndView modaljudge() {
-//		mav = new ModelAndView();
-//		System.out.println("111");
-//		List<String> jlist = aDao.getjudgemodal();
-//		System.out.println("222");
-//		return null;
-//	}
-	
-	
-}//class End
+	// 심사 승인 서비스(rest)
+	public String judgeApproved(String number, String type) {
+		System.out.println("am승인은 오니?");
+		String json = null;
+		if (type.equals("house")) {	//가져온 타입이 house면 house
+			boolean result = aDao.houseApproved(number);
+			System.out.println(result);
+			if (result == true) {
+				List<Adminhouse> hlist = aDao.getHouseList();
+				json = new Gson().toJson(hlist);
+			}
+		} else if (type.equals("food")) { //가져온 타입이 food면 food
+			boolean result = aDao.foodApproved(number);
+			System.out.println(result);
+			if (result == true) {
+				List<Adminfood> hlist = aDao.getFoodList();
+				json = new Gson().toJson(hlist);
+			}
+		}
+		return json;
+	}//judgeApproved End
+
+	// 심사 거절 서비스
+	public String judgerefuse(String number, String type) {
+		System.out.println("am거절은 오니?");
+
+		String json = null;
+		if (type.equals("house")) { //가져온 타입이 house면 house
+			boolean result = aDao.housefuse(number);
+			System.out.println(result);
+			if (result == true) {
+				List<Adminhouse> hlist = aDao.getHouseList();
+				json = new Gson().toJson(hlist);
+			}
+		} else if (type.equals("food")) { //가져온 타입이 food면 food
+			boolean result = aDao.foodfuse(number);
+			System.out.println(result);
+			if (result == true) { 
+				List<Adminfood> hlist = aDao.getFoodList();
+				json = new Gson().toJson(hlist);
+			}
+		}
+
+		return json;
+	}//judgerefuse End
+
+}// class End
