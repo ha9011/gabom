9a5481da14235cd73d93d14fbe72e9ab4453bb59
ha <ha90111@naver.com>
2020-03-26 11:02:15 +0900

    socket midy footer-alarm

--- src/main/java/icia/project/gabom/websocket/AlramChattingHandler.java
+++ src/main/java/icia/project/gabom/websocket/AlramChattingHandler.java
@@ -24,10 +24,15 @@ public class AlramChattingHandler extends HttpSessionHandshakeInterceptor{
 		HttpServletRequest req =  ssreq.getServletRequest();
         	
 		// HttpSession 에 저장된 이용자의 아이디를 추출하는 경우  (세션으로 아이디 가져오기)
-		String userID = (String)req.getSession().getAttribute("userID"); // 세션에서 값을 가져오는거
-		System.out.println("HttpSession에 저장된 userID:"+userID);
-        attributes.put("userID", userID);
-
+				String userID = (String)req.getSession().getAttribute("userID"); // 세션에서 값을 가져오는거
+				System.out.println("HttpSession에 저장된 userID:"+userID);
+				
+		        if(userID==null) {
+		        	
+		        }else {
+		        	attributes.put("userID", userID);
+		            	
+		        }
 
 		return super.beforeHandshake(request, response, wsHandler, attributes);
 	
