cc3e509b6d3a5c8a7f5ca6ef09cfd865c9971f4e
Kim-seongjun <01090524238@naver.com>
2020-04-01 20:33:56 +0900

    qna글작성 진행중

--- src/main/java/icia/project/gabom/dao/IServiceDao.java
+++ src/main/java/icia/project/gabom/dao/IServiceDao.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Select;
 
 import icia.project.gabom.dto.Adminnotices;
 import icia.project.gabom.dto.Qnaboard;
+import icia.project.gabom.dto.Serviceqna;
 
 public interface IServiceDao {
 	@Select("select all_notices_number,all_notices_title,all_notices_body,all_notices_date,all_notices_views from all_notices_board")
@@ -13,4 +14,8 @@ public interface IServiceDao {
 	
 	@Select("select qna_number,qna_title,qna_body,qna_member_id,qna_date from qna_board")
 	List<Qnaboard> getQnaList();
+
+	boolean getqnawrite(String title, String content);
+
+	List<Serviceqna> getqnalist();
 }
