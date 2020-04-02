0b2c84470e77eec02630fb2e2523203fb1d8cfdf
Kim-seongjun <01090524238@naver.com>
2020-04-02 18:02:41 +0900

    04.02고객센터완

--- src/main/java/icia/project/gabom/RestServiceCenter.java
+++ src/main/java/icia/project/gabom/RestServiceCenter.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 
 import icia.project.gabom.dao.IServiceDao;
-import icia.project.gabom.dto.Serviceqna;
+import icia.project.gabom.dto.Qnaboard;
 
 
 
@@ -19,20 +19,22 @@ public class RestServiceCenter {
 	@Autowired
 	private IServiceDao sDao;
 	
+	//qna 글작성
 	@PostMapping(value = "/qnawrite", produces = "text/plain;charset=UTF-8")
-	public String qnawrite(@RequestParam("title") String title, @RequestParam("content") String content) {
+	public String qnawrite(@RequestParam("title") String title, @RequestParam("content") String content, @RequestParam("member") String member) {
 		System.out.println("레스트컨트룰러 qna작성모달");
 		System.out.println("qna작성 제목 : " + title);
 		System.out.println("qna작성 내용 : " + content);
-		boolean result = sDao.getqnawrite(title, content);
+		System.out.println("qna작성자 : " + member);
+		boolean result = sDao.getqnawrite(title, content, member);
 		System.out.println("글작성 성공?" + result);
 
-		List<Serviceqna> nlist = sDao.getqnalist(); // 전체공지 정보 출력
-		System.out.println("noticeslist=" + nlist);
+		List<Qnaboard> nlist = sDao.getQnaList(); // 전체qna 정보 출력
+		System.out.println("qnalist=" + nlist);
 
 		String json = new Gson().toJson(nlist);
 		System.out.println(json);
 
-		return null;
+		return json;
 	}
 }
